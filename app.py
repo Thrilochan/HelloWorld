941693c4e053cefe124bcfe1946ee1c848cfa313
Thrilochan <34273281+Thrilochan@users.noreply.github.com>
2017-12-06 15:52:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def webhookresult(req):
   if __name__ == '__main__'
      port=int(os.getenv('PORT',5000))
      print("App running on port %d" %(port))
-     app.run(debug=False, port=port, host='0.0.0.0')
+     app.run(debug=True, port=port, host='0.0.0.0')
     
