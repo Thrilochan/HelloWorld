01310125f1c9e753a9e4c344303ca1867b11ca82
Thrilochan <34273281+Thrilochan@users.noreply.github.com>
2017-12-06 12:55:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,30 @@ from flask import make_response
 app = Flask(__name__);
 
 @app.route('/webhook', methods=["POST"])
+def webhook():
+    req=request.get_json(silent=True, force=True)
+    req=json.dumbs(req,indent=4)
+    res=webhookresult(req)
+    res=json.dumps(res,indent=4)
+    print(res)
+    r=make_respone(res)
+    return r
+def webhookresult(req):
+    if req.get("result").get("action") != "BankInterest"
+      return{}
+    res=req.get("result")
+    parameters=res.get("parameters")
+    name=parameters.get("bankname")
+    interest={"HDFC":"10.99","ICICI":"11.0","SBI":"12","AXIS":"11.5"}
+    speech="The interest rate of "+name+" is "+str(interest(name))
+    return{
+      "speech":speech,
+      "displayText":speech,
+      "source":"bankinterestapp"
+    }
+  
+  if __name__ == '__main__'
+     port=int(os.getenv('PORT',5000))
+     print("App running on port %d" %(port))
+     app.run(debug=false, port=port, host='0.0.0.0')
+    
