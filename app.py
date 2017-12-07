e4701c6721225a2e619850775ac2e1d8e20690fd
Thrilochan <34273281+Thrilochan@users.noreply.github.com>
2017-12-07 11:34:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,24 +21,22 @@ def webhook():
     r.headers['Content-Type']='application/json'
     return r
 
- """def webhookresult(req):
-    if req.get("result").get("action") != "BankInterest"
-      return{}
+def webhookresult(req):
+    if req.get("result").get("action") != "BankInterest":
+     return{}
     res=req.get("result")
     parameters=res.get("parameters")
     name=parameters.get("bankname")
     interest={'HDFC':'10.99','ICICI':'11.0','SBI':'12','AXIS':'11.5'}
-    speech="The interest rate of "+name+" is "+str(interest(name))"""
-    def webhookresult(req):
-        return
-        {
-         "speech":"No banks available",
-         "displayText":"No banks available",
-         "source":"bankinterestapp"
-        }
+    speech="The interest rate of "+name+" is "+str(interest(name))
+    return
+    {
+    "speech":speech,
+    "displayText":speech,
+    "source":"bankinterestapp"
+    }
   
-  if __name__ == '__main__'
-     port=int(os.getenv('PORT',5000))
-     print("App running on port %d" % port)
-     app.run(debug=True, port=port, host='0.0.0.0')
-    
+if __name__ == '__main__':
+    port=int(os.getenv('PORT',5000))
+    print("App running on port %d" % port)
+    app.run(debug=True, port=port, host='0.0.0.0')
