138406d6b5895eff46cdd8c370afb8a81af2f89f
Thrilochan <34273281+Thrilochan@users.noreply.github.com>
2017-12-07 16:06:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,33 +20,32 @@ from flask import make_response
 
 app = Flask(__name__)
 
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
-    print(json.dumbs(req, indent=4))
-    res = webhookresult(req)
-    res = json.dumps(res, indent=4)
-    r = make_respone(res)
+    req=request.get_json(silent=True, force=True)
+    print(json.dumps(req, indent=4))
+    res=webhookresult(req)
+    res=json.dumps(res, indent=4)
+    r= make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
-
 def webhookresult(req):
     if req.get("result").get("action") != "BankInterest":
-        return {}
-    res = req.get("result")
-    parameters = res.get("parameters")
-    name = parameters.get("bankname")
-    interest = {'HDFC': '10.99','ICICI': '11.0','SBI': '12','AXIS': '11.5'}
-    speech = "The interest rate of " + name + " is "+str(interest[name])
-    return
-    {
-      'speech': speech,
-      'displayText': speech,
-      'source': 'bankinterestapp'
+        return{}
+    res=req.get("result")
+    parameters=res.get("parameters")
+    name=parameters.get("bankname")
+    interest={'HDFC':'10.99','ICICI':'11.0','SBI':'11.5'}
+    speech="The interest rate of "+name+" is "+str(interest[name])
+    return{
+        'speech':speech
+        'dispalyText':speech
+        'source':'BankInterestApp'
     }
+int __name__ == '__main__':
+    port=int(os.getenv('PORT', 5000))
+    print("App is running on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
+              
     
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print ("App running on port %d" % port)
-    app.run(debug=false, port=port, host='0.0.0.0')
+    
