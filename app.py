4eed388dfe54d982133cb464effffaa9dfa1a627
Thrilochan <34273281+Thrilochan@users.noreply.github.com>
2017-12-07 15:18:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
+import urllib
 import json
 import os
 
@@ -18,32 +13,39 @@ from flask import make_response
 
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
-def webhook():  
-    req=request.get_json(silent=True, force=True)
-    print(json.dumbs(req,indent=4))
-    res=webhookresult(req)
-    res=json.dumps(res,indent=4)
-    r=make_respone(res)
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print json.dumbs(req, indent=4)
+    res = webhookresult(req)
+    res = json.dumps(res, indent=4)
+    r = make_respone(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
+
 def webhookresult(req):
-    if req.get("result").get("action") != "BankInterest":
-     return{}
-    res=req.get("result")
-    parameters=res.get("parameters")
-    name=parameters.get("bankname")
-    interest={'HDFC':'10.99','ICICI':'11.0','SBI':'12','AXIS':'11.5'}
-    speech="The interest rate of "+name+" is "+str(interest(name))
+    if req.get('result').get('action') != 'BankInterest':
+        return {}
+    res = req.get('result')
+    parameters = res.get('parameters')
+    name = parameters.get('bankname')
+    interest = {
+        'HDFC': '10.99',
+        'ICICI': '11.0',
+        'SBI': '12',
+        'AXIS': '11.5',
+        }
+    speech = 'The interest rate of ' + name + ' is ' \
+        + str(interest(name))
     return
-    {
-    "speech":speech,
-    "displayText":speech,
-    "source":"bankinterestapp"
-    }
-  
+
+    {'speech': speech, 'displayText': speech,
+     'source': 'bankinterestapp'}
+
+
 if __name__ == '__main__':
-    port=int(os.getenv('PORT',5000))
-    print("App running on port %d" % port)
-    app.run(debug=True, port=port, host='0.0.0.0')
+    port = int(os.getenv('PORT', 5000))
+    print 'App running on port %d' % port
+    app.run(debug=false, port=port, host='0.0.0.0')
